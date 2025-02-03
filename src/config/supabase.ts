e238b428a9e905556a1e02dbb59b56b99181bb4a
Fern-05 <braydonfurneaux@gmail.com>
2025-02-02 22:36:13 -0500

    Update supabase.ts

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,5 +1,5 @@
 // Get the URL and anon key from your Supabase project settings
 export const supabaseConfig = {
-  url: "https://your-project-url.supabase.co",
+  url: "https://xagxrwfyaphuwufhvoyl.supabase.co",
   anonKey: "your-anon-key"
 };
